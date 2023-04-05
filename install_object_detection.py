3cab2c4904763c36b6863e56cd35cecf3231c837
Alexandra <alexandragoffova@gmail.com>
2023-04-05 03:51:16 +0100

    test

--- install_object_detection.py
+++ install_object_detection.py
@@ -1,11 +1,12 @@
 import subprocess
+import os
 
 # Clone the TensorFlow models repository
 subprocess.run(['rm', '-rf', './models'])
 subprocess.run(['git', 'clone', 'https://github.com/tensorflow/models.git'])
 
 # Install the object_detection library
-subprocess.run(['cd', 'models/research'])
+os.chdir('models/research')
 subprocess.run(['protoc', 'object_detection/protos/*.proto', '--python_out=.'])
 subprocess.run(['cp', 'object_detection/packages/tf2/setup.py', '.'])
 subprocess.run(['python3', '-m', 'pip', 'install', '--use-feature=2020-resolver', '.'])
